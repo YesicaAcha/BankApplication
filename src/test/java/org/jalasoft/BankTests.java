8548dc1e893b25fa2652ecf5f9055f932a29fa08
yesica <yesica.acha@jalasoft.com>
2019-11-25 17:02:15 -0400

    BankServices and BankAccount

--- src/test/java/org/jalasoft/BankTests.java
+++ src/test/java/org/jalasoft/BankTests.java
@@ -19,25 +19,24 @@ public class BankTests {
         int accountOne = bank.newAccount();
         int accountTwo = bank.newAccount();
         int accountThree = bank.newAccount();
-
+        
         //Assert
         assertEquals(0, accountZero);
         assertEquals(1, accountOne);
         assertEquals(2, accountTwo);
         assertEquals(3, accountThree);
-
     }
 
     @Test
-    public void depositAmountToAccount() {
+    public void getAccountGivenTheAccountNumber() {
         //Arrange
         Bank bank = new Bank();
         int accountZero = bank.newAccount();
-
+        
         //Act
-        bank.deposit(accountZero, 100);
+        BankAccount accountOne = bank.getBankAccount(accountZero);
         
         //Assert
-        assertEquals(100, bank.getBalance(accountZero));
+        assertEquals(accountZero, accountOne.getAccountNumber());
     }
 }
